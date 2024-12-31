4cf63c2326d830fd68e1152d541b788b0c1ffbf2
Aashu-destined <aashutoshnamdeo3@gmail.com>
2024-12-31 21:51:53 +0530

    working fine, but refinment for easy to use is needed.

---
+++ main.py
@@ -0,0 +1,63 @@
+
+def encrypt(msg):
+    words = msg.split(" ")
+    encrypt_msg = []
+    for word in words:
+        if(len(word)>3):
+            r1 = "tyi"
+            r2 = "nxl"
+            tempword = r1+ word[1:]+ word[0]+ r2
+            encrypt_msg.append(tempword)
+        else:
+            encrypt_msg.append(word[::-1]) # to reverse the element of the list
+
+    encrypt_msg = " ".join(encrypt_msg)
+    return encrypt_msg
+def decrypt(msg):
+    words = msg.split(" ")
+    decrypt_msg = []
+    for word in words:
+        if(len(word)>3):
+            # tempword = word[-4]+ word[3:-4]     # it is the subtitute of the below two lines, but it is complecated to understand.
+            tempword = word[3:-3]  # Strip "tyi" from the start and "nxl" from the end
+            tempword = tempword[-1] + tempword[:-1]  # Move the last character to the front
+            decrypt_msg.append(tempword)
+        else:
+            decrypt_msg.append(word[::-1]) # to reverse the element of the list
+
+    decrypt_msg = " ".join(decrypt_msg)
+    return decrypt_msg
+
+def salect(msg):            #will ask to encrypt or decrypt the message
+    option = int(input("To Encrypt the msg please enter 1\nTo Dycrypt the msg please enter 0  "))
+    if(option == 0):
+        message = (encrypt(msg))
+    elif(option == 1):
+        message = (decrypt(msg))
+    else:
+        message = "Envalid option. Please enter 1 for encryption or 0 for decryption."
+
+    print(message)
+
+def run(msg):
+    # option = int(input("To Encrypt the msg please enter 1\nTo Dycrypt the msg please enter 0  "))
+    print(salect(msg))   
+
+
+# if("__name__" == "__main__"):
+
+msg = input("Enter your message: ")
+# run(msg)
+
+# option = input("To Encrypt the msg please enter 1\nTo Dycrypt the msg please enter 0  ")
+# print(salect(option,msg))
+
+# option = int(input("To Encrypt the msg please enter 1\nTo Dycrypt the msg please enter 0  "))
+# if(option == 0):
+#     print(encrypt(msg))
+# elif(option == 1):
+#     print(decrypt(msg))
+# else:
+#     message = "Envalid option. Please enter 1 for encryption or 0 for decryption."
+print(encrypt(msg))
+print(decrypt(encrypt(msg)))
